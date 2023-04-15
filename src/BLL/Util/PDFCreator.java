e6f0ee92baf33f1cccdc408e3359872e356441b7
jonasjakobsen1152 <jonasjakobsen1152@gmail.com>
2023-04-15 16:19:24 +0200

    Temporary pdf fun

--- src/BLL/Util/PDFCreator.java
+++ src/BLL/Util/PDFCreator.java
@@ -1,11 +1,60 @@
 package BLL.Util;
 
+
+import DAL.DB.MyDatabaseConnector;
 import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfDocument;
 import com.itextpdf.text.pdf.PdfWriter;
 
-import java.io.FileOutputStream;
+import java.awt.*;
+import java.io.*;
+import java.sql.Connection;
+import java.sql.SQLException;
 
 public class PDFCreator {
-    Document document = new Document();
-    //PdfWriter pdfWriter = PdfWriter.getInstance()
+
+    private void createPDF(){
+        try {
+            Document document = new Document();
+            PdfWriter.getInstance(document, new FileOutputStream("Ticket.pdf"));
+
+
+            document.open();
+
+            //Adding to the pdf
+            Paragraph paragraph = new Paragraph("This is a test from jones");
+            document.add(paragraph);
+
+            document.close();
+
+            File file = new File("Ticket.pdf");
+
+            Desktop.getDesktop().open(file);
+
+
+
+
+        } catch (DocumentException | FileNotFoundException e) {
+            System.out.println(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+
+    public static void main(String[] args) throws SQLException {
+
+        //MyDatabaseConnector databaseConnector = new MyDatabaseConnector();
+
+        PDFCreator pdfCreator = new PDFCreator();
+
+        pdfCreator.createPDF();
+
+    }
+
+
+
 }
